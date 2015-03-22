5cb7008944c48d4035ca3afa8f9d48b1de3f5cc6
libbyschuknight <libbysk@gmail.com>
2015-03-22 17:11:17 +1300

    ruby basics

--- hello.rb
+++ hello.rb
@@ -1,6 +1,7 @@
-print "Please enter your name: "
-name = gets
-# puts "Hello #{name}!"
-print "Please enter your last name: "
-last_name = gets
-puts name + last_name
+name = "Libby"
+wife = "Anya"
+cats = "Jessie and Misty"
+string = "Hello.\nMy name is #{name}. My wife is #{wife}.\nOur cats are called #{cats}.\nI love Ruby!"
+
+puts string
+
