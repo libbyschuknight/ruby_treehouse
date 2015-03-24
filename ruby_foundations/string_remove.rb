5120b9b40cce63b235fc14704e6eb23b45ec706d
libbyschuknight <libbysk@gmail.com>
2015-03-25 08:12:09 +1300

    ruby files

---
+++ ruby_foundations/string_remove.rb
@@ -0,0 +1,7 @@
+string = puts "Write a sentence with punc."
+string = gets
+puts string
+string.chop!
+puts string
+string.reverse!
+puts string
---
+++ ruby_foundations/strings.rb
@@ -0,0 +1,87 @@
+# puts first_name = "Libby"
+# puts last_name = "Schumacher-Knight"
+# puts first_name + last_name
+# puts first_name + " " + last_name
+# puts first_name * 3
+# puts first_name << " " << last_name
+# puts " "
+# puts first_name
+# puts last_name
+# puts first_name[0]
+# name = gets
+# puts name
+
+
+# puts string = "hello world\n"
+# puts string.inspect
+# puts string.chomp
+# puts string.chop
+# puts string.include?("world")
+# puts string.include?("Libby")
+# puts string
+# puts string.replace("Libby")
+# puts string
+# puts string.index("y")
+
+
+puts string = "Hello world"
+puts string.reverse
+puts string.upcase
+puts string.downcase
+puts string.swapcase
+puts string.length
+puts string.size
+print string.split(" ")
+string = "Hello world how are you?"
+print string.split(" ")
+
+puts string.concat("Another string")
+
+string = "Hello world"
+string.reverse!
+puts string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
